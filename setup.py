b4c877a0cc037fc58213418403b0aa5d4989ba86
h0nda <41300123+h0nde@users.noreply.github.com>
2020-11-12 10:52:33 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sel-requests",
+    #version="0.0.1",
+    author="h0nda",
+    author_email="1@1.com",
+    description="Send requests using selenium",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/h0nde/sel-requests",
+    packages=setuptools.find_packages(),
+    classifiers=[
+    ],
+    install_requires=[
+        "requests",
+        "selenium"
+    ],
+    python_requires='>=3.6',
+)
